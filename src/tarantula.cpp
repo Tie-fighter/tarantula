949df818856592b4806458991e86d801c1438bec
thomas <thomas@thomas-steinbrenner.net>
2012-12-14 05:49:26 +0100

    time to sleep

--- src/tarantula.cpp
+++ src/tarantula.cpp
@@ -8,9 +8,10 @@
 
 #include <cv.h>
 
-#include <opencv2/opencv.hpp>
 #include <iostream>
 #include <vector>
+#include <opencv2/opencv.hpp>
+#include <opencv2/stitching/stitcher.hpp>
 
 using namespace std;
 using namespace cv;
@@ -22,21 +23,21 @@ static void usage() {
 
 int main(int argc, char** argv) {
 
-    VideoCapture cap;
+  VideoCapture cap;
 
-    if (argc > 1) {
-      for (int i = 1; i < argc; i++) {
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
 
-        // -f <filename>
-        if (string(argv[i]) == "-f") {
-          string filename = string(argv[i+1]);
-          cap.open(filename);
-          i++;
+      // -f <filename>
+      if (string(argv[i]) == "-f") {
+        string filename = string(argv[i+1]);
+        cap.open(filename);
+        i++;
 
-          if (cap.isOpened() != true) {
-            cerr << "Error: \"" << filename << "\" could not be opened.\n exiting..." << endl;
-          }
+        if (cap.isOpened() != true) {
+          cerr << "Error: \"" << filename << "\" could not be opened.\n exiting..." << endl;
         }
+      }
 
         // noise
 //        else if (string(argv[i]) == 
@@ -62,15 +63,65 @@ int main(int argc, char** argv) {
     return -1;
   }
 
+  Mat pano;
 
-  Mat frame, foreground, background;
-  BackgroundSubtractorMOG2 bg(300, 16, false);
+  cap >> pano;
+
+  namedWindow("pano", CV_WINDOW_NORMAL);
+
+  for (;;) {
+
+    Mat frame1, frame2, frame3;
+    Mat result;
+    
+    cap >> frame1;
+    cap >> frame2;
+    cap >> frame3;
+
+    vector<Mat> vec;
+    vec.push_back(pano);
+    vec.push_back(frame1);
+ 
+    cout << "frame1: " << frame1.cols << "x" << frame1.rows << endl;
+    cout << "pano: " << pano.cols << "x"<< pano.rows << endl;
+
+    Stitcher stitcher = Stitcher::createDefault(true);
+    Stitcher::Status status = stitcher.stitch(vec, result);
+
+    cout << status << endl;
+    cout << "result: " << result.cols << "x" << result.rows << endl;
+
+    cout << result.total() << " " <<  pano.total() << endl;
+
+    if (result.total() >= pano.total()) {
+      result.copyTo(pano);
+      cout << "pew pew" << endl;
+    }
+ 
+    imshow("pano", pano);
+
+    waitKey(100);
+    cout << "---" << endl;
+
+  }
+
+  return 0;
+
+}
+  
+
+
+/*
+
+  Mat frame, foreground, background, contour;
+  BackgroundSubtractorMOG2 bg(3000, 16, false);
 
   std::vector<std::vector<cv::Point> > contours;
 
   namedWindow("frame", CV_WINDOW_NORMAL);
   namedWindow("foreground", CV_WINDOW_NORMAL);
   namedWindow("background", CV_WINDOW_NORMAL);
+//  namedWindow("contour", CV_WINDOW_NORMAL);
 
   for (;;) {
     cap >> frame;
@@ -79,18 +130,23 @@ int main(int argc, char** argv) {
       break;
     }
 
-    bg.operator() (frame, foreground);
+    bg.operator() (frame, foreground, 0.001);
     bg.getBackgroundImage(background); 
 
-    erode(foreground, foreground, Mat(), Point(-1, -1), 1);
-    dilate(foreground, foreground, Mat(), Point(-1, -1), 1);
+//    erode(foreground, foreground, Mat(), Point(-1, -1), 2);
+//    dilate(foreground, foreground, Mat(), Point(-1, -1), 2);
 
+    frame.copyTo(contour);
+    contour &= Scalar(0, 0, 0);
     findContours(foreground, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-    drawContours(frame, contours, -1, Scalar(0,0,255), 2);
+    drawContours(contour, contours, -1, Scalar(0,0,255), -1);
+    addWeighted(frame, 1, contour, 0.5, 0, frame);
+    drawContours(frame, contours, -1, Scalar(0,0,0), 1);
 
     imshow("frame", frame);
     imshow("foreground", foreground);
     imshow("background", background);
+//    imshow("contour", contour);
 
     int c = waitKey(30);
     if (c == 'q' || c == 'Q' || (c & 255) == 27) {
@@ -102,3 +158,4 @@ int main(int argc, char** argv) {
 
 }
 
+*/
